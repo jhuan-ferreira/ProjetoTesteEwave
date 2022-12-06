5afe59fb2d2e00e00340e95f24f7b0da8c3e4804
Jhuan Ferreira Silva <ruan.2604@outlook.com>
2022-12-06 18:44:14 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoTeste/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace ProjetoTeste
+{
+    partial class frmTeste
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtMedia = new System.Windows.Forms.TextBox();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(143, 29);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtMedia
+            // 
+            this.txtMedia.Location = new System.Drawing.Point(134, 90);
+            this.txtMedia.Name = "txtMedia";
+            this.txtMedia.ReadOnly = true;
+            this.txtMedia.Size = new System.Drawing.Size(100, 22);
+            this.txtMedia.TabIndex = 1;
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Location = new System.Drawing.Point(78, 93);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(50, 17);
+            this.lblMedia.TabIndex = 2;
+            this.lblMedia.Text = "Media:";
+            // 
+            // frmTeste
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 176);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.txtMedia);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "frmTeste";
+            this.Text = "Teste Ewave";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtMedia;
+        private System.Windows.Forms.Label lblMedia;
+    }
+}
+
---
+++ ProjetoTeste/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ProjetoTeste
+{
+    public partial class frmTeste : Form
+    {
+        public frmTeste()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> Tabela = new List<int>() { 987, 236, 547, 72, 138, 790, 83, -435, 234, 34 };
+
+            txtMedia.Text = Convert.ToString(RetornaMedia(Tabela));
+
+        }
+
+        public double RetornaMedia(List<int> tabela)
+        {
+            if (tabela.Count < 0 || tabela.Count >= 100)
+            {
+                return -1;
+            }
+
+            double somaMedia = 0;
+            int divisores;
+
+            List<int> listNumeros = new List<int>();
+
+            foreach (var item in tabela)
+            {
+                    divisores = 0;
+
+                    for (int i = 1; i <= item; i++)
+                    {
+                        if (item % i == 0)
+                        {
+                            divisores++;
+                        }
+                    }
+
+                if (item > 0 && divisores != 2)
+                {
+                    listNumeros.Add(item);
+
+                }
+            }
+
+            foreach (var numero in listNumeros)
+            {
+                somaMedia =+ numero;
+            }
+
+            return somaMedia;
+        }
+    }
+}
